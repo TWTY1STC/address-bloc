d261da7282bc05e9a6f7bce02d47653133c88c2e
LizaN <liza.njuguna@gmail.com>
2016-03-02 19:41:33 +0000

    Add menu controller

--- address_bloc.rb
+++ address_bloc.rb
@@ -1,1 +1,10 @@
+#driver program
+require_relative 'controllers/menu_controller'
+
+menu = MenuController.new #creates new MenuController when AddressBook starts
+
+system "clear"  #clears CLI
 puts "Welcome to AddressBloc!"
+
+
+menu.main_menu  #display menu
