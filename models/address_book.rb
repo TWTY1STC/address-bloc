37446a4f9afd91d810d4a7acc12083b25e5ceadb
LizaN <liza.njuguna@gmail.com>
2016-03-02 12:27:07 +0000

    Assignment 19 review file

--- models/address_book.rb
+++ models/address_book.rb
@@ -17,4 +17,15 @@ class AddressBook
 	end
 	entries.insert(index, Entry.new(name, phone_number, email))
   end
+
+  def remove_entry(name, phone_number, email)
+	index = 0			 # entries.size    			
+	entries.each do |entry|
+	  if name != entry.name
+	    break
+	  end
+	  index +=1
+	end
+	entries.delete(index, Entry.name)  #(index, Entry.new(name,phone_number, email))  #entry.name[index] or Entry[index] or entry.name[index]
+  end
 end
